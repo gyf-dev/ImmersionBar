a61a1832104ce6665590d9de2ae0fde127133fb1
geyifeng <geyifeng@wakfo.com>
2018-12-25 17:20:12 +0800

    update demo

--- sample/src/main/java/com/gyf/immersionbar/MyApp.java
+++ sample/src/main/java/com/gyf/immersionbar/MyApp.java
@@ -2,9 +2,6 @@ package com.gyf.immersionbar;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
-import com.tencent.bugly.crashreport.CrashReport;
-
 /**
  * @author geyifeng
  * @date 2017/8/4
@@ -13,10 +10,5 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        CrashReport.initCrashReport(getApplicationContext(), "31a5f1f394", false);
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            return;
-        }
-        LeakCanary.install(this);
     }
 }
